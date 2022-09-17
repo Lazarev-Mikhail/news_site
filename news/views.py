606aa2324d83b8ddaa992eea47a9cd5d7d16ee6b
paris <mikhaillazarev007@gmail.com>
2022-09-17 03:52:21 +0300

    1)creating pagination 2)settings pagination in index.html

--- news/views.py
+++ news/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from news.models import News, Category
+from django.core.paginator import Paginator
+
 
 
 # Create your views here.
@@ -9,13 +11,14 @@ from news.models import News, Category
 class NewsListView(ListView):
     model = News
     template_name = 'news/index.html'
+    paginate_by = 20
 
     def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
         context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
         context['last_news'] = News.objects.all()[:4]
-        context['other_news'] = News.objects.all()[4:]
+        paginator = Paginator(News.objects.all()[4:], self.paginate_by)
+        page = self.request.GET.get('page')
         context['categories'] = Category.objects.all()
+        context['page_counter'] = range(paginator.num_pages)
         return context
 
